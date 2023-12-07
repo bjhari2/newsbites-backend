ba870709451e085a3ab883ceb9c72d3fb998a79c
bjhari2 <121340541+bjhari2@users.noreply.github.com>
2023-12-07 17:27:39 +0530

    Changed port number

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const bodyParser = require('body-parser')
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const app = express();
 app.use(bodyParser.text())
